10c7264609f63de082a522f2de8fa16b982df300
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-14 09:21:10 -0500

    test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,28 +30,27 @@ function createNavBar() {
 
 createNavBar();
 
-const imagePaths= [
-   '../im'
-]; 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const gallery = document.getElementById('imageGallery');
-
-imagePaths.forEach((path, index) => {
-    const img = document.createElement('img');
-    img.src = path;
-    img.alt = `Image ${index + 1}`;
-    img.onclick = () => openModal(path);
-    gallery.appendChild(img);
-});
-
-function openModal(imageSrc) {
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modalImg');
-
-    modal.style.display = 'block';
-    modalImg.src = imageSrc;
+function changeSlide(n) {
+  showSlides(slideIndex += n);
 }
 
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("gallery-image");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
