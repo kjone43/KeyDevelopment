abf11c390bce742ffcc9d7977e79647b80efb83f
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-13 22:47:03 -0500

    update java

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,3 +29,24 @@ function createNavBar() {
 
 // Call the function to create the navigation bar
 createNavBar();
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Change image every 2 seconds (2000 milliseconds)
+}
