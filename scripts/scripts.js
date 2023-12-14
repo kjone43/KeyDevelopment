87aaea2bb1ee918cb79d6b788a94e7a0c9556198
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-14 11:28:18 -0500

    test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,27 +30,29 @@ function createNavBar() {
 
 createNavBar();
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function changeSlide(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  const slides = document.getElementsByClassName("gallery-image");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+  
+    function changeSlide(n) {
+      showSlides(slideIndex += n);
+    }
+  
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+  
+    function showSlides(n) {
+      const slides = document.getElementsByClassName("gallery-image");
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slides[slideIndex - 1].style.display = "block";
+    }
+  });
