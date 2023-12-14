0da7fba155dd31e141e17d18d28f628c256b7118
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-14 13:35:23 -0500

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,27 +26,27 @@ function createNavBar() {
 
     navbar.appendChild(ul);
 }
-
-
 createNavBar();
 
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
     let slides = document.getElementsByClassName("gallery-image");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-function changeSlide(n) {
-    slideIndex += n;
-    showSlides();
+    slides[slideIndex].style.display = "block";
 }
