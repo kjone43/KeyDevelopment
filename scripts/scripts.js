c0949491146f7e6ed8d833e0b6281aa4106086fc
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-13 22:19:42 -0500

    committed

---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+// Navigation bar data
+const navLinks = [
+    { text: 'Home', link: '#' },
+    { text: 'About', link: '#about' },
+    { text: 'Services', link: '#services' },
+    { text: 'Contact', link: '#contact' }
+];
+
+// Function to create the navigation bar
+function createNavBar() {
+    const navbar = document.getElementById('navbar');
+
+    const ul = document.createElement('ul');
+    ul.classList.add('navigation');
+
+    navLinks.forEach(linkData => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        a.textContent = linkData.text;
+        a.href = linkData.link;
+
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    navbar.appendChild(ul);
+}
+
+// Call the function to create the navigation bar
+createNavBar();
