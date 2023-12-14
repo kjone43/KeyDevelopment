969b44b64815bb1b718c022550b290abb89d4040
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-14 15:22:55 -0500

    test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,40 +29,26 @@ function createNavBar() {
 createNavBar();
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    let slideIndex = 0;
-    const slides = document.querySelectorAll('.gallery-image');
-  
-    function showSlide(n) {
-      if (n >= slides.length) {
-        slideIndex = 0;
-      }
-      if (n < 0) {
-        slideIndex = slides.length - 1;
-      }
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-      }
-      slides[slideIndex].style.display = 'block';
-    }
-  
-    function changeSlide(n) {
-      showSlide(slideIndex += n);
-    }
-  
-    // Show the first image initially
-    showSlide(slideIndex);
-  
-    // Add event listeners to prev and next buttons
-    const prevButton = document.querySelector('.prev');
-    const nextButton = document.querySelector('.next');
-  
-    prevButton.addEventListener('click', function() {
-      changeSlide(-1);
-    });
-  
-    nextButton.addEventListener('click', function() {
-      changeSlide(1);
-    });
-  });
-  
+let slideIndex = 0;
+const slides = document.querySelectorAll('.gallery-image');
+
+function showSlide(n) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex += n;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides[slideIndex].style.display = 'block';
+}
+
+function changeSlide(n) {
+  showSlide(n);
+}
+
+// Show the first image initially
+showSlide(slideIndex);
