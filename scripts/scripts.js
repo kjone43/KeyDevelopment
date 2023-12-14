17293cfe7faad9358272c0edff2aa218498bd701
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-14 13:30:39 -0500

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,32 +30,23 @@ function createNavBar() {
 
 createNavBar();
 
-document.addEventListener("DOMContentLoaded", function() {
-    let slideIndex = 1;
-    showSlides(slideIndex);
-  
-    function changeSlide(n) {
-      showSlides(slideIndex += n);
-    }
-  
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-  
-    function showSlides() {
-        const slides = document.querySelectorAll(".gallery-image");
+let slideIndex = 0;
+showSlides();
 
-        if (slideIndex >= slides.length) {
-            slideIndex = 0;
-        }
-        if (slideIndex < 0) {
-            slideIndex = slides.length - 1;
-        }
-
-        slides.forEach((slide) => {
-            slide.classList.remove("active");
-        });
-
-        slides[slideIndex].classList.add("active");
+function showSlides() {
+    let slides = document.getElementsByClassName("gallery-image");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-});
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
