44745142c13f6d8043375ff1473f8769469ca7a1
KyAnna Jones <kyannajones958@yahoo.com>
2023-12-14 15:08:52 -0500

    test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,26 +29,40 @@ function createNavBar() {
 createNavBar();
 
 
-
-let slideIndex = 0;
-const slides = document.querySelectorAll('.gallery-image');
-
-function showSlide(n) {
-    if (n >= slides.length) {
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll('.gallery-image');
+  
+    function showSlide(n) {
+      if (n >= slides.length) {
         slideIndex = 0;
-    }
-    if (n < 0) {
+      }
+      if (n < 0) {
         slideIndex = slides.length - 1;
-    }
-    for (let i = 0; i < slides.length; i++) {
+      }
+      for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
+      }
+      slides[slideIndex].style.display = 'block';
     }
-    slides[slideIndex].style.display = 'block';
-}
-
-function changeSlide(n) {
-    showSlide(slideIndex += n);
-}
-
-// Show the first image initially
-showSlide(slideIndex);
+  
+    function changeSlide(n) {
+      showSlide(slideIndex += n);
+    }
+  
+    // Show the first image initially
+    showSlide(slideIndex);
+  
+    // Add event listeners to prev and next buttons
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+  
+    prevButton.addEventListener('click', function() {
+      changeSlide(-1);
+    });
+  
+    nextButton.addEventListener('click', function() {
+      changeSlide(1);
+    });
+  });
+  
